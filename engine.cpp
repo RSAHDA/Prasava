0aaf783f4d331669741c18f0e2c676619fa3baaf
RagavN12 <sriragav@protonmail.com>
2022-05-28 20:50:51 -0400

    greet added

--- engine.cpp
+++ engine.cpp
@@ -2,20 +2,36 @@
 #include <fstream>
 #include <string>
 # include <windows.h>
-# include <ctime>
+# include <ctime>// nenaw
 
 using namespace std;
 
-string greet(){
-    time_t now = time(0);
+void print(const string arg)
+{
+    cout << arg << endl;
+}
+
+void greet(){
+    time_t now = time(nullptr);
     tm *currtime = localtime(&now);
 
+    int mhour = currtime->tm_hour;
+    int day = currtime->tm_mday;
+    int month = currtime->tm_mon;
+    int mints = currtime->tm_min;
+    int year = currtime->tm_year;
+
+    if (mhour > 5 and mhour < 12) {
+        cout << "Good Morning!\n";
+    } else if (mhour >= 12 and mhour < 16){
+        print("Good Afternoon!");
+    } else {
+        print("Good Evening!");
+    }
 }
 
 int main(){
-
-    cout << greet();
-
+    greet();
 }
 
 
